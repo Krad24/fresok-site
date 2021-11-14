0417795e556068f6a1ec44083ea1ffca643c7fea
Krad24 <marhipov240692@gmail.com>
2021-11-15 00:16:57 +0200

    готовые блоки, нужно ещё доработать

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function(){
-    
+    $('.slider__items').slick({
+        prevArrow: '<button type="button" class="slick-arrows arrow-prev"><img src="../images/icon/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-arrows arrow-next"><img src="../images/icon/arrow-right.svg" alt=""></button>',
+        infinite: false,
+    });
+
+    var mixer = mixitup('.tovars__cards');
+
 })
